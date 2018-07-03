7984a5c017b207e9a19398da55a0d039430e109e
Andrew Paradi <account+github@adrw.ch>
2018-07-03 14:27:50 -0400

    Up to date README

--- src/components/HelloComponent.jsx
+++ src/components/HelloComponent.jsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 
 const Hello = (props) => (
     <div>
-      <h1>It's <strong>HomePage</strong> y'allllllllllllllll</h1>
       <h1>This React site was built by {props.author} at {props.company}</h1>
     </div>
 )
